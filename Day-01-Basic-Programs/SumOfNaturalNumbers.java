dc0d92ae2cf17cc421ef102010231a4d3704f85e
Bhoomika Yadav <25112cn089@glbitm.ac.in>
2026-06-15 14:07:32 +0530

    Added SumOfNaturalNumbers program
    This program calculates the sum of the first N natural numbers entered by the user.

--- Day-01-Basic-Programs/SumOfNaturalNumbers.java
+++ Day-01-Basic-Programs/SumOfNaturalNumbers.java
@@ -1,1 +1,24 @@
+import java.util.*;
+
+public class SumOfNaturalNumbers {
+
+    public static void main(String args []) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int sum = 0;
+        for( int number = 1; number <= n; number ++) {
+            sum = sum + number;
+        }
+        System.out.println(sum);
+    }
+
+}
+ 
+
+
+
+
+
 
