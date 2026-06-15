e6fb118ad0293c4ab4eeae71f25079625287cfb1
Bhoomika Yadav <25112cn089@glbitm.ac.in>
2026-06-15 16:44:32 +0530

    Added MultiplicationTable program
    This program prints the multiplication table of a number entered by the user.

--- Day-01-Basic-Programs/MultiplicationTable.java
+++ Day-01-Basic-Programs/MultiplicationTable.java
@@ -1,1 +1,20 @@
+import java.util.*;
+
+public class MultiplicationTable {
+
+    public static void main (String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for ( int number = 1; number < 11; number++) {
+              System.out.println( n * number);
+        
+        }
+        sc.close();
+
+    }  
+
+}
+
 
