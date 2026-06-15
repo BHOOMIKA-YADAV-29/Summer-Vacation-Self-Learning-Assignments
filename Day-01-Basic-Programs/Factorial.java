4c1a4fc7169f724221298641deecf939238828c6
Bhoomika Yadav <25112cn089@glbitm.ac.in>
2026-06-15 23:34:01 +0530

    Added Factorial program
    This program calculates the factorial of a number entered  by the user.

--- Day-01-Basic-Programs/Factorial.java
+++ Day-01-Basic-Programs/Factorial.java
@@ -1,1 +1,21 @@
+import java.util.*;
 
+public class Factorial {
+
+    public static void main (String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        long factorial = 1;
+
+        for ( int number = 1; number <= n; number++ ) {
+            factorial = factorial * number;
+            
+        }
+        System.out.println(factorial);
+        sc.close();
+
+    }
+
+}
