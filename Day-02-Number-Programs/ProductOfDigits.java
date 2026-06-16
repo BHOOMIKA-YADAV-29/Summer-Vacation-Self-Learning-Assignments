8fb74063908f770dab58880fe70233b7a0ff759e
Bhoomika Yadav <25112cn089@glbitm.ac.in>
2026-06-16 12:35:09 +0530

    Added ProductOfDigits program
    This program calculates the product of the digits of a number entered by the user.

--- Day-02-Number-Programs/ProductOfDigits.java
+++ Day-02-Number-Programs/ProductOfDigits.java
@@ -1,1 +1,21 @@
+import java.util.*;
 
+public class ProductOfDigits {
+
+    public static void main (String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        int product = 1;
+
+        while (number > 0) {
+            product = number % 10 * product;
+            number = number / 10;
+        }
+        System.out.println(product);
+
+        sc.close();
+
+    }
+    
+}
