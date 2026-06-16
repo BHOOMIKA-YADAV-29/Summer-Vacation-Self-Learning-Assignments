c4b3070cd3ff48240bc04e785b6b24189bd419d5
Bhoomika Yadav <25112cn089@glbitm.ac.in>
2026-06-16 12:07:04 +0530

    Added Reverse program
    This program prints the reverse of a number entered by the user.

--- Day-02-Number-Programs/Reverse.java
+++ Day-02-Number-Programs/Reverse.java
@@ -1,1 +1,22 @@
+import java.util.*;
 
+public class Reverse { 
+
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        int reverse = 0;
+
+        while (number > 0) {
+            int digit = number % 10;
+            reverse = digit + reverse*10;
+            number = number/10;
+        } 
+
+        System.out.println(reverse);
+        sc.close();
+
+    }
+    
+}
