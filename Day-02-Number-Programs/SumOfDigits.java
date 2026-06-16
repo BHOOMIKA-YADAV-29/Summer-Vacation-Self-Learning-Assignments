8e451035fe352a12dd8622bd5e47cb4e49d556b9
Bhoomika Yadav <25112cn089@glbitm.ac.in>
2026-06-16 11:24:19 +0530

    Added SumOfDigits program
    This program calculates the sum of digits of a number entered by the user.

--- Day-02-Number-Programs/SumOfDigits.java
+++ Day-02-Number-Programs/SumOfDigits.java
@@ -1,1 +1,22 @@
+import java.util.*;
 
+public class SumOfDigits {
+
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+
+        int sum = 0;
+
+        while ( number > 0) {
+            sum = number % 10 + sum;
+            number = number / 10;
+            
+        }
+        System.out.println(sum);
+
+        sc.close();
+    }
+    
+}
