feef298db7b428fb65ce11e3f74d2f8f23f77a67
Bhoomika Yadav <25112cn089@glbitm.ac.in>
2026-06-19 16:53:13 +0530

    Added Palindrome program
    This program checks whether the number entered by the user is a palindrome number or not.

--- Day-02-Number-Programs/Palindrome.java
+++ Day-02-Number-Programs/Palindrome.java
@@ -1,1 +1,28 @@
+import java.util.*;
 
+public class Palindrome {
+
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number :");
+        int number = sc.nextInt();
+        int ReverseNum = 0;
+        int OriginalNumber = number;
+           
+        while (number > 0 ) {
+            int digit = number % 10;
+            ReverseNum = digit + ReverseNum*10;
+            number = number/10;
+        }
+            
+        if ( OriginalNumber == ReverseNum ) {
+            System.out.println( "Given Number is a Palindrome Number");
+        }
+        else {
+            System.out.println("Given Number is not a Palindrome Number");
+        } 
+        sc.close();
+    }
+}
