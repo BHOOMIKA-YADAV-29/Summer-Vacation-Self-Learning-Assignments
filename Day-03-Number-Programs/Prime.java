394098f06ae1b91206b838c2d586356e6f38f240
Bhoomika Yadav <25112cn089@glbitm.ac.in>
2026-06-19 17:57:04 +0530

    Added Prime program
    This program checks whether the number entered by the user is a Prime number or not.

--- Day-03-Number-Programs/Prime.java
+++ Day-03-Number-Programs/Prime.java
@@ -1,1 +1,29 @@
+import java.util.*;
 
+public class Prime {
+
+    public static void main(String args[]) {
+        // Check whether given number is a prime number or not
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number :");
+        int number = sc.nextInt();
+        int count = 0;
+
+        for ( int i = 1; i <= number; i++) {
+            if ( number % i == 0) {
+                
+                count ++;
+            }     
+
+        }
+        if ( count ==2 ) {
+            System.out.println("Given number is a Prime number");
+        }
+        else {
+            System.out.println("Given number is not a Prime number");
+        }
+        sc.close();
+    }
+    
+}
