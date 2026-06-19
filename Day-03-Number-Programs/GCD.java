d1d5737c3817b41872178ba9af41c39c26593c9f
Bhoomika Yadav <25112cn089@glbitm.ac.in>
2026-06-19 18:28:25 +0530

    Added GCD program
    This program calculates the GCD of two numbers.

--- Day-03-Number-Programs/GCD.java
+++ Day-03-Number-Programs/GCD.java
@@ -1,1 +1,36 @@
+import java.util.*;
 
+public class GCD {
+
+    public static void main(String args[]) {
+        // Write a program to find GCD of two numbers
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter number1 :");
+        System.out.println("Enter number2 :");
+        int number1 = sc.nextInt();
+        int number2 = sc.nextInt();
+        int gcd = 1;
+        // gcd is the greatest number which can divide both of the numbers
+        if ( number1 > number2 ) {
+            for( int i = 1; i <= number1; i++) {
+                if ( number1 % i==0 && number2 % i==0 ) {
+
+                    gcd = i;
+                }
+            }
+        }
+        else
+        {
+            for( int i = 1; i <= number2; i++) {
+                if (number1 % i==0 && number2 % i==0 ) {
+
+                    gcd = i;
+                }
+            }
+        }
+        System.out.println("GCD of given numbers is :" + gcd);
+        sc.close();
+    }
+
+}
