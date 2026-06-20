0eda725d4c026d08f985a07271e3682b415af12c
Bhoomika Yadav <25112cn089@glbitm.ac.in>
2026-06-20 15:25:26 +0530

    Added Fibonacci program
    This program generates Fibonacci series.

--- Day-04-Number-Programs/Fibonacci.java
+++ Day-04-Number-Programs/Fibonacci.java
@@ -1,1 +1,32 @@
+import java.util.*;
 
+public class Fibonacci 
+{
+        public static void main(String args[]) 
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number of terms in fibonacci series ");
+        int number = sc.nextInt();
+        int firstTerm = 0; 
+        int secondTerm = 1;
+        int nextTerm;
+
+        System.out.print( firstTerm + " ");
+
+        if ( number > 1 ) 
+        {
+            System.out.print( secondTerm + " ");
+        }
+
+        for ( int i = 3; i <= number; i++ ) 
+        {
+            nextTerm = firstTerm + secondTerm;
+            firstTerm = secondTerm;
+            secondTerm = nextTerm;
+            System.out.print( nextTerm + " ");
+        }
+        sc.close();
+
+    }
+}
