4c5847849840f58826d3883ae2abd9055d9f0f96
Bhoomika Yadav <25112cn089@glbitm.ac.in>
2026-06-20 13:18:14 +0530

    Added PrimeInRange program
    This program prints prime numbers in a range.

--- Day-03-Number-Programs/PrimeInRange.java
+++ Day-03-Number-Programs/PrimeInRange.java
@@ -1,1 +1,31 @@
+import java.util.*;
 
+public class PrimeInRange {
+
+    public static void main(String args[]) {
+        // Write a program to print prime numbers in a range.
+        System.out.println("Enter the number upto which prime numbers should be printed ");
+
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+
+        // Exclude 0 & 1 as they are not prime numbers
+        for ( int i = 2; i <= number; i++ ) {
+            int count = 0;
+            
+            for( int j = 1; j <= i; j++ ) {
+                if( i % j ==0 )  {
+
+                    count++;
+                }
+            }
+            if( count == 2 ) {
+                System.out.print( i+ " ");
+            }
+        }
+
+            sc.close();
+            
+    }
+    
+}
