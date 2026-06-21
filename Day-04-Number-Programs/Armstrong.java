80297a7db8de476cee5317816b6937e69e441f52
Bhoomika Yadav <25112cn089@glbitm.ac.in>
2026-06-21 09:59:05 +0530

    Added Armstrong program
    This program checks whether the number entered by the user is an armstrong number or not.

--- Day-04-Number-Programs/Armstrong.java
+++ Day-04-Number-Programs/Armstrong.java
@@ -1,1 +1,40 @@
+import java.util.*;
+
+public class Armstrong {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number : ");
+        int number = sc.nextInt();
+
+        int originalNumber = number; 
+        int numberOfDigits = 0; 
+        int sum = 0;
+
+        while ( number > 0 ) 
+        {
+            numberOfDigits++;
+            number /= 10;
+        }
+
+        number = originalNumber;
+
+        while ( number > 0 ) {
+            sum += Math.pow( number % 10 , numberOfDigits );
+            number /= 10;
+        }
+
+        if ( originalNumber == sum ) 
+        {
+            System.out.println("Given number is an armstrong number ");
+        } 
+        else {
+            System.out.println("Given number is not an armstrong number ");
+        }
+
+        sc.close();
+    }   
+}
+    
 
