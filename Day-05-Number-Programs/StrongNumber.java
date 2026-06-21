8f040b061bfe2f76ad4b4d8ff54cbfac23d98df5
Bhoomika Yadav <25112cn089@glbitm.ac.in>
2026-06-21 17:31:21 +0530

    Added StrongNumber program
    This program checks whether the number entered by the user is a strong number or not.

--- Day-05-Number-Programs/StrongNumber.java
+++ Day-05-Number-Programs/StrongNumber.java
@@ -1,1 +1,37 @@
+import java.util.*;
 
+// Write a program to Check strong number.
+public class StrongNumber {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number:");
+        int number = sc.nextInt();
+        int originalNumber = number;
+        int sum = 0;
+
+        while ( number > 0 ) {
+
+            int digit = number % 10;
+            int factorial = 1;
+
+            for ( int i = 1; i <= digit; i++ ) {
+                factorial *= i;
+            }
+
+            sum += factorial;
+            number /= 10;
+        }
+
+        if ( sum == originalNumber ) {
+            System.out.println("It is a Strong Number");
+        } 
+        else {
+            System.out.println("It is not a Strong Number");
+        }
+
+        sc.close();
+    }
+}
