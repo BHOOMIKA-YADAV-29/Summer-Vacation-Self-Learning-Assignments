85e4d324feeba6f56b452dfd6d496a7b0e3854e0
Bhoomika Yadav <25112cn089@glbitm.ac.in>
2026-06-21 17:16:35 +0530

    Added PerfectNumber program
    This program checks whether the number entered by the user is a perfect number or not.

--- Day-05-Number-Programs/PerfectNumber.java
+++ Day-05-Number-Programs/PerfectNumber.java
@@ -1,1 +1,28 @@
+import java.util.*;
 
+// Write a program to Check perfect number.
+public class PerfectNumber {
+    
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number:");
+        int number = sc.nextInt();
+        int sum = 1;
+
+        for ( int i = 2; i < number; i++ ) {
+            if (number % i == 0) {
+                sum += i;
+            }
+        }
+
+        if ( number > 1 && sum == number ) {
+            System.out.println("It is a Perfect Number");
+        } else {
+            System.out.println("It is not a Perfect Number");
+        }
+
+        sc.close();
+    }
+}
