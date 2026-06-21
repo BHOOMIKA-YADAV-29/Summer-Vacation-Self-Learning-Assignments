2a84b9f928bc9592dee9333b4f8eb8766445cbc6
Bhoomika Yadav <25112cn089@glbitm.ac.in>
2026-06-21 17:59:09 +0530

    Added LargestPrimeFactor program
    This program finds the largest prime factor of a number entered by the user.

--- Day-05-Number-Programs/LargestPrimeFactor.java
+++ Day-05-Number-Programs/LargestPrimeFactor.java
@@ -1,1 +1,43 @@
+import java.util.*;
 
+// Write a program to Find largest prime factor.
+public class LargestPrimeFactor {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number:");
+        int number = sc.nextInt();
+
+        if ( number <= 1 ) {
+            System.out.println("Given number has no prime factors");
+            sc.close();
+            return;
+        }
+
+        int largestPrimeFactor = 1;
+
+        for ( int i = 2; i <= number; i++ ) {
+
+            if ( number % i == 0 ) {
+
+                int count = 0;
+
+                for ( int j = 1; j <= i; j++ ) {
+                    if ( i % j == 0 ) {
+                        count++;
+                    }
+                }
+
+                if ( count == 2 ) {
+                    largestPrimeFactor = i;
+                }
+            }
+        }
+
+        System.out.println("Largest Prime Factor of " + number + " is :" + largestPrimeFactor );
+
+        sc.close();
+    }
+}
