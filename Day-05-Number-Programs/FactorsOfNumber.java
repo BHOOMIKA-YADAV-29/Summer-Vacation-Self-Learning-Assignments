fd992f3e5fc22df04721564672139a9d727e79ad
Bhoomika Yadav <25112cn089@glbitm.ac.in>
2026-06-21 17:40:41 +0530

    Added FactorsOfNumber program
    This program prints factors of a number entered by the user.

--- Day-05-Number-Programs/FactorsOfNumber.java
+++ Day-05-Number-Programs/FactorsOfNumber.java
@@ -1,1 +1,23 @@
+import java.util.*;
 
+// Write a program to Print factors of a number.
+public class FactorsOfNumber {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number:");
+        int number = sc.nextInt();
+
+        System.out.println("Factors of " + number + " are :");
+
+        for ( int i = 1; i <= number; i++ ) {
+            if ( number % i == 0 ) {
+                System.out.print(i + " ");
+            }
+        }
+
+        sc.close();
+    }
+}
