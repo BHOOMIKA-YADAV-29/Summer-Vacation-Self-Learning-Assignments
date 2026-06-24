bce0fc244474a427740a19244f7950315884b4ea
Bhoomika Yadav <25112cn089@glbitm.ac.in>
2026-06-24 13:41:47 +0530

    Added StarPyramidPattern program
    This program prints Star Pyramid Pattern.

--- Day-10-Pattern-Programs/StarPyramidPattern.java
+++ Day-10-Pattern-Programs/StarPyramidPattern.java
@@ -1,1 +1,38 @@
+import java.util.*;
+
+/* Write a program to Print star pyramid.
+     *
+    ***
+   *****
+  *******
+ *********
+ */
+
+public class StarPyramidPattern {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the height of the star pyramid :");
+        int height = sc.nextInt();
+
+        for ( int i = 1; i <= height; i++ ) {
+
+            // To print spaces
+            for ( int j = 1; j <= height - i; j++ ) {
+                System.out.print(" ");
+            }
+
+            // To print stars
+            for ( int j = 1; j <= 2 * i - 1; j++ ) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
 
