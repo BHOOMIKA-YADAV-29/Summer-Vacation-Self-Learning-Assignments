793a275745f907617849bd3b29c56a10e3ca147e
Bhoomika Yadav <25112cn089@glbitm.ac.in>
2026-06-24 12:40:57 +0530

    Added ReverseNumberTriangle program
    This program prints Reverse Number Triangle pattern.

--- Day-09-Pattern-Programs/ReverseNumberTriangle.java
+++ Day-09-Pattern-Programs/ReverseNumberTriangle.java
@@ -1,1 +1,32 @@
+import java.util.*;
 
+/* Write a program to Print reverse number
+triangle.
+12345
+1234
+123
+12
+1 
+*/
+
+public class ReverseNumberTriangle {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the height of Reverse Number Triangle :");
+        int rows = sc.nextInt();
+
+        for ( int i = rows; i >= 1; i-- ) {
+
+            for ( int j = 1; j <= i; j++ ) {
+                System.out.print(j);
+            }
+
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
