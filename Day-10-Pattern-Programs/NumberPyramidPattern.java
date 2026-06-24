e63d2b82a27c9f7939ec0277206e8a248ca64309
Bhoomika Yadav <25112cn089@glbitm.ac.in>
2026-06-24 14:45:18 +0530

    Added NumberPyramidPattern program
    This program prints Number Pyramid Pattern.

--- Day-10-Pattern-Programs/NumberPyramidPattern.java
+++ Day-10-Pattern-Programs/NumberPyramidPattern.java
@@ -1,1 +1,42 @@
+import java.util.*;
 
+/* Write a program to Print number pyramid.
+       1
+      121
+     12321
+    1234321
+   123454321 
+*/
+
+public class NumberPyramidPattern {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the height of the number pyramid :");
+        int height = sc.nextInt();
+
+        for ( int i = 1; i <= height; i++ ) {
+
+            // Print spaces
+            for ( int j = 1; j <= height - i; j++ ) {
+                System.out.print(" ");
+            }
+
+            // Print ascending numbers
+            for ( int j = 1; j <= i; j++ ) {
+                System.out.print(j);
+            }
+
+            // Print descending numbers
+            for ( int j = i - 1; j >= 1; j-- ) {
+                System.out.print(j);
+            }
+
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
