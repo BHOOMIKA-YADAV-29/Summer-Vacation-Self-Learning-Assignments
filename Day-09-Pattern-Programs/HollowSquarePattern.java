2f6f437579cc07375bb86e6e5158c8691e9d3dc2
Bhoomika Yadav <25112cn089@glbitm.ac.in>
2026-06-24 13:04:20 +0530

    Added HollowSquarePattern program
    This program prints Hollow Square Pattern.

--- Day-09-Pattern-Programs/HollowSquarePattern.java
+++ Day-09-Pattern-Programs/HollowSquarePattern.java
@@ -1,1 +1,37 @@
+import java.util.*;
 
+/* Write a program to Print hollow square pattern.
+   *****
+   *   *
+   *   *
+   *   *
+   ***** 
+*/
+
+public class HollowSquarePattern {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the size of the square:");
+        int size = sc.nextInt();
+
+        for ( int i = 1; i <= size; i++ ) {
+
+            for ( int j = 1; j <= size; j++ ) {
+
+                if ( i == 1 || i == size || j == 1 || j == size ) {
+                    System.out.print("*");
+                } 
+                else {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
