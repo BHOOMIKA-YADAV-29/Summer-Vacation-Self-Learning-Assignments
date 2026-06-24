58dd07a60df3d2b9513493619aea550b6c977a29
Bhoomika Yadav <25112cn089@glbitm.ac.in>
2026-06-24 12:34:51 +0530

    Added ReverseStarPattern program
    This program prints Reverse Star Pattern.

--- Day-09-Pattern-Programs/ReverseStarPattern.java
+++ Day-09-Pattern-Programs/ReverseStarPattern.java
@@ -1,1 +1,31 @@
+import java.util.*;
 
+/* Write a program to Print reverse star pattern.
+   *****
+   ****
+   ***
+   **
+   *            
+*/
+
+public class ReverseStarPattern {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number of rows:");
+        int rows = sc.nextInt();
+
+        for (int i = rows; i >= 1; i--) {
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
