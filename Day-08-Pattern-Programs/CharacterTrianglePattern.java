0c8b96b013a0aa915c9ef007214697e62d6d139d
Bhoomika Yadav <25112cn089@glbitm.ac.in>
2026-06-24 12:19:59 +0530

    Added CharacterTrianglePattern program
    This program prints Character Triangle pattern.

--- Day-08-Pattern-Programs/CharacterTrianglePattern.java
+++ Day-08-Pattern-Programs/CharacterTrianglePattern.java
@@ -1,1 +1,33 @@
+import java.util.*;
 
+/* Write a program to Print character triangle.
+   A
+   AB
+   ABC
+   ABCD
+   ABCDE */
+
+public class CharacterTrianglePattern {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the height of the character triangle :");
+        int rows = sc.nextInt();
+
+        for ( int i = 1; i <= rows; i++ ) {
+
+            char letter = 'A';
+
+            for ( int j = 1; j <= i; j++ ) {
+                System.out.print(letter);
+                letter++;
+            }
+
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
