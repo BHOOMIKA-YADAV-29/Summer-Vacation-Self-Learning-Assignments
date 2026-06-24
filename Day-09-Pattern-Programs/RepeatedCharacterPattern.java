be1f619bf134ac3a5fd5149f26341afd15045dfd
Bhoomika Yadav <25112cn089@glbitm.ac.in>
2026-06-24 12:47:08 +0530

    Added RepeatedCharacterPattern program
    This program prints Repeated Character Pattern.

--- Day-09-Pattern-Programs/RepeatedCharacterPattern.java
+++ Day-09-Pattern-Programs/RepeatedCharacterPattern.java
@@ -1,1 +1,34 @@
+import java.util.*;
 
+/* Write a program to Print repeated character
+pattern.
+A
+BB
+CCC
+DDDD
+EEEEE */
+
+public class RepeatedCharacterPattern {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the height of the repeated character pattern :");
+        int rows = sc.nextInt();
+
+        char letter = 'A';
+
+        for ( int i = 1; i <= rows; i++ ) {
+
+            for ( int j = 1; j <= i; j++ ) {
+                System.out.print(letter);
+            }
+            letter++;
+
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
