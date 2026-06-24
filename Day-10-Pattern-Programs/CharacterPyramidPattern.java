cafac210c87998fe6f1e565b068627c9f0647e88
Bhoomika Yadav <25112cn089@glbitm.ac.in>
2026-06-24 14:52:40 +0530

    Added CharacterPyramidPattern program
    This program prints Character Pyramid Pattern.

--- Day-10-Pattern-Programs/CharacterPyramidPattern.java
+++ Day-10-Pattern-Programs/CharacterPyramidPattern.java
@@ -1,1 +1,42 @@
+import java.util.*;
 
+/* Write a program to Print character pyramid. 
+       A
+      ABA
+     ABCBA
+    ABCDCBA
+   ABCDEDCBA 
+*/
+
+public class CharacterPyramidPattern {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the height of the character pyramid :");
+        int height = sc.nextInt();
+
+        for ( int i = 1; i <= height; i++ ) {
+
+            // Print spaces
+            for ( int j = 1; j <= height - i; j++ ) {
+                System.out.print(" ");
+            }
+
+            // Print ascending characters
+            for ( char letter = 'A'; letter < 'A' + i; letter++ ) {
+                System.out.print(letter);
+            }
+
+            // Print descending characters
+            for ( char letter = (char) ('A' + i - 2); letter >= 'A'; letter-- ) {
+                System.out.print(letter);
+            }
+
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
