09e527b339c1b342eb90516d765c2fdcda5a777e
Bhoomika Yadav <25112cn089@glbitm.ac.in>
2026-06-26 18:26:45 +0530

    Added FunctionSum program
    This program defines a function to calculate the sum of two numbers.

--- Day-11-Functions/FunctionSum.java
+++ Day-11-Functions/FunctionSum.java
@@ -1,1 +1,26 @@
+import java.util.*;
 
+/* Write a program to Write function to find sum of two numbers. */
+
+public class FunctionSum {
+
+    public static int findSum(int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
+    }
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the first number :");
+        int firstNumber = sc.nextInt();
+
+        System.out.println("Enter the second number :");
+        int secondNumber = sc.nextInt();
+
+        int sum = findSum( firstNumber, secondNumber );
+
+        System.out.println("The sum of the two numbers is : " + sum);
+
+        sc.close();
+    }
+}
