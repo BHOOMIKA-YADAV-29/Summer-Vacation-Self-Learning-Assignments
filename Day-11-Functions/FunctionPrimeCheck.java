c89ba660f500f1818e57781efad9d7c2030eefe7
Bhoomika Yadav <25112cn089@glbitm.ac.in>
2026-06-26 18:48:29 +0530

    Added FunctionPrimeCheck program
    This program defines a function to check whether the number entered by the user is a prime number or not.

--- Day-11-Functions/FunctionPrimeCheck.java
+++ Day-11-Functions/FunctionPrimeCheck.java
@@ -1,1 +1,37 @@
+import java.util.*;
 
+// Write a program to Write function to check prime.
+
+
+public class FunctionPrimeCheck {
+
+    // This function checks whether a number is prime.
+    public static void checkPrime(int number) {
+
+        if (number <= 1) {
+            System.out.println("The given number is not a Prime Number");
+            return;
+        }
+
+        for ( int i = 2; i * i <= number; i++ ) {
+            if (number % i == 0) {
+                System.out.println("The given number is not a Prime Number");
+                return;
+            }
+        }
+
+        System.out.println("The given number is a Prime Number");
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number :");
+        int number = sc.nextInt();
+
+        checkPrime(number);
+
+        sc.close();
+    }
+}
