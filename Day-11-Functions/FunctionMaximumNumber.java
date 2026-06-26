b8b5c4c7db5927ff733531df0b514d39a3b4ba1b
Bhoomika Yadav <25112cn089@glbitm.ac.in>
2026-06-26 18:37:38 +0530

    Added FunctionMaximumNumber program
    This program defines a function to find the maximum number.

--- Day-11-Functions/FunctionMaximumNumber.java
+++ Day-11-Functions/FunctionMaximumNumber.java
@@ -1,1 +1,33 @@
+import java.util.*;
 
+// Write a program to Write function to find maximum.
+
+public class FunctionMaximumNumber {
+
+    public static int findMaximum( int firstNumber, int secondNumber ) {
+
+        if ( firstNumber > secondNumber ) {
+            return firstNumber;
+        } 
+        else {
+            return secondNumber;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the first number :");
+        int firstNumber = sc.nextInt();
+
+        System.out.println("Enter the second number :");
+        int secondNumber = sc.nextInt();
+
+        int maximum = findMaximum( firstNumber, secondNumber );
+
+        System.out.println("The maximum number is : " + maximum);
+
+        sc.close();
+    }
+}
