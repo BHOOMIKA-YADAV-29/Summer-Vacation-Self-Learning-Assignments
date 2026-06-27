361b8088fb9430b816841672f63a45016ef92960
Bhoomika Yadav <25112cn089@glbitm.ac.in>
2026-06-27 18:04:05 +0530

    Added SecondLargestElement program
    This program finds the second largest element in an array.

--- Day-14-Arrays/SecondLargestElement.java
+++ Day-14-Arrays/SecondLargestElement.java
@@ -1,1 +1,48 @@
+import java.util.*;
 
+// Write a program to find the Second largest element.
+
+public class SecondLargestElement {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the size of the array :");
+        int size = sc.nextInt();
+
+        int[] arr = new int[size];
+
+        System.out.println("Enter the array elements :");
+
+        for ( int i = 0; i < size; i++ ) {
+            arr[i] = sc.nextInt();
+        }
+
+        int largest = arr[0];
+        int secondLargest = Integer.MIN_VALUE;
+
+        // Find largest
+        for ( int i = 0; i < size; i++ ) {
+            if ( arr[i] > largest ) {
+                largest = arr[i];
+            }
+        }
+
+        // Find second largest
+        for ( int i = 0; i < size; i++ ) {
+            if ( arr[i] > secondLargest && arr[i] != largest ) {
+                secondLargest = arr[i];
+            }
+        }
+
+        if ( secondLargest == Integer.MIN_VALUE ) {
+            System.out.println("There is no second largest element");
+        } 
+        else {
+            System.out.println("The second largest element is : " + secondLargest);
+        }
+
+        sc.close();
+    }
+}
