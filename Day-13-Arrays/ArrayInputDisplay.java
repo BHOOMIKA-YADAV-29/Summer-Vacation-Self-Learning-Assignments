881bfb2f3e7d863868c9b2df8fca99203e8ea816
Bhoomika Yadav <25112cn089@glbitm.ac.in>
2026-06-27 13:25:40 +0530

    Added ArrayInputDisplay program
    This program inputs and displays the elements of an array.

--- Day-13-Arrays/ArrayInputDisplay.java
+++ Day-13-Arrays/ArrayInputDisplay.java
@@ -1,1 +1,32 @@
+import java.util.*;
 
+// Write a program to Input and display array.
+
+public class ArrayInputDisplay {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the size of the array :");
+        int size = sc.nextInt();
+
+        int[] array = new int[size];
+        
+        // input array
+        System.out.println("Enter the array elements :");
+
+        for (int i = 0; i < size; i++) {
+            array[i] = sc.nextInt();
+        }
+
+        // display array
+        System.out.println("The array elements are :");
+
+        for (int i = 0; i < size; i++) {
+            System.out.print(array[i] + " ");
+        }
+
+        sc.close();
+    }
+}
