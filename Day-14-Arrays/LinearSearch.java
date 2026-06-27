e22b3e1362817465908158f947135fe62fbbdcd2
Bhoomika Yadav <25112cn089@glbitm.ac.in>
2026-06-27 17:36:17 +0530

    Added LinearSearch program
    This program searches an element in an array using linear search.

--- Day-14-Arrays/LinearSearch.java
+++ Day-14-Arrays/LinearSearch.java
@@ -1,1 +1,37 @@
+import java.util.*;
 
+// Write a program to Linear search.
+
+public class LinearSearch {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number of elements in the array :");
+        int length = sc.nextInt();
+
+        int[] arr = new int[length];
+
+        System.out.println("Enter the array elements :");
+
+        for ( int i = 0; i < length; i++ ) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Enter the element to search :");
+        int key = sc.nextInt();
+
+        for (int i = 0; i < length; i++) {
+            if ( arr[i] == key ) {
+                System.out.println("Element found at index : " + i);
+                sc.close();
+                return;
+            }
+        }
+
+        System.out.println("Element not found");
+
+        sc.close();
+    }
+}
