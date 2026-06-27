8de396ab6e75e69d9711f71c8878d0d6e411fac4
Bhoomika Yadav <25112cn089@glbitm.ac.in>
2026-06-27 14:01:27 +0530

    Added ArrayEvenOddCount program
    This program counts even and odd elements in an array.

--- Day-13-Arrays/ArrayEvenOddCount.java
+++ Day-13-Arrays/ArrayEvenOddCount.java
@@ -1,1 +1,36 @@
+import java.util.*;
 
+// Write a program to Count even and odd elements.
+
+public class ArrayEvenOddCount {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the size of the array :");
+        int size = sc.nextInt();
+
+        int[] array = new int[size];
+        int evenCount = 0;
+        int oddCount = 0;
+
+        System.out.println("Enter the array elements :");
+
+        for ( int i = 0; i < size; i++ ) {
+            array[i] = sc.nextInt();
+
+            if ( array[i] % 2 == 0 ) {
+                evenCount++;
+            } 
+            else {
+                oddCount++;
+            }
+        }
+
+        System.out.println("Number of even elements : " + evenCount);
+        System.out.println("Number of odd elements : " + oddCount);
+
+        sc.close();
+    }
+}
