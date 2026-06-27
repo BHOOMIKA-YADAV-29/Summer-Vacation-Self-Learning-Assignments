c7cb5b124c362423f63daca8adc385ef1ca36a6f
Bhoomika Yadav <25112cn089@glbitm.ac.in>
2026-06-27 12:46:57 +0530

    Added FunctionPalindrome program
    This program defines a function to check whether the number entered by the user is a palindrome number or not.

--- Day-12-Functions/FunctionPalindrome.java
+++ Day-12-Functions/FunctionPalindrome.java
@@ -1,1 +1,36 @@
+import java.util.*;
 
+// Write a program to Write function for palindrome.
+
+public class FunctionPalindrome {
+
+    public static void checkPalindrome(int number) {
+
+        int originalNumber = number;
+        int reverse = 0;
+
+        while ( number > 0 ) {
+            reverse = reverse * 10 + (number % 10);
+            number /= 10;
+        }
+
+        if ( originalNumber == reverse ) {
+            System.out.println("The given number is a Palindrome");
+        } 
+        else {
+            System.out.println("The given number is not a Palindrome");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number :");
+        int number = sc.nextInt();
+
+        checkPalindrome(number);
+
+        sc.close();
+    }
+}
