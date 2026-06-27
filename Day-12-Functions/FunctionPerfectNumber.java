7f38478f6d268dfbeec87eff173049e24ed615f6
Bhoomika Yadav <25112cn089@glbitm.ac.in>
2026-06-27 13:11:13 +0530

    Added FunctionPerfectNumber program
    This program defines a function to check whether the number entered by the user is a perfect number or not.

--- Day-12-Functions/FunctionPerfectNumber.java
+++ Day-12-Functions/FunctionPerfectNumber.java
@@ -1,1 +1,41 @@
+import java.util.*;
 
+// Write a program to Write function for perfect number.
+
+public class FunctionPerfectNumber {
+
+    public static void checkPerfectNumber(int number) {
+
+        if ( number <= 1 ) {
+            System.out.println("The given number is not a Perfect Number");
+            return;
+        }
+
+        int sum = 1;
+
+        for ( int i = 2; i < number; i++ ) {
+            if ( number % i == 0 ) {
+                sum += i;
+            }
+        }
+
+        if ( sum == number ) {
+            System.out.println("The given number is a Perfect Number");
+        } 
+        else {
+            System.out.println("The given number is not a Perfect Number");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        int number = sc.nextInt();
+
+        checkPerfectNumber(number);
+
+        sc.close();
+    }
+}
