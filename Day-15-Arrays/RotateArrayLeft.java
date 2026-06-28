92a40eec4ad5b67ed3e69a47630ab4d17b2b5ab4
Bhoomika Yadav <25112cn089@glbitm.ac.in>
2026-06-29 00:17:30 +0530

    Added RotateArrayLeft program
    This program rotates the elements of an array one position to the left.

--- Day-15-Arrays/RotateArrayLeft.java
+++ Day-15-Arrays/RotateArrayLeft.java
@@ -1,1 +1,38 @@
+import java.util.*;
 
+// Write a program to Rotate array left.
+
+public class RotateArrayLeft {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the size of the array :");
+        int size = sc.nextInt();
+
+        int[] arr = new int[size];
+
+        System.out.println("Enter the array elements :");
+
+        for ( int i = 0; i < size; i++ ) {
+            arr[i] = sc.nextInt();
+        }
+
+        int first = arr[0];
+
+        for ( int i = 0; i < size - 1; i++ ) {
+            arr[i] = arr[i + 1];
+        }
+
+        arr[size - 1] = first;
+
+        System.out.println("Array after left rotation :");
+
+        for (int i = 0; i < size; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
+    }
+}
