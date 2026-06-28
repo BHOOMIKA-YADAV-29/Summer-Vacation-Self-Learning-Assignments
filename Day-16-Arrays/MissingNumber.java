506a2faec1ccbcef069a01a200410ddcd8bf2fe4
Bhoomika Yadav <25112cn089@glbitm.ac.in>
2026-06-29 00:32:40 +0530

    Added MissingNumber program
    This program finds the missing number in an array.

--- Day-16-Arrays/MissingNumber.java
+++ Day-16-Arrays/MissingNumber.java
@@ -1,1 +1,35 @@
+import java.util.*;
 
+// Write a program to Find missing number in array.
+
+public class MissingNumber {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the size of the array :");
+        int size = sc.nextInt();
+
+        int[] arr = new int[size];
+
+        System.out.println("Enter the array elements :");
+
+        for ( int i = 0; i < size; i++ ) {
+            arr[i] = sc.nextInt();
+        }
+
+        int expectedSum = (size + 1) * (size + 2) / 2;
+        int actualSum = 0;
+
+        for ( int i = 0; i < size; i++ ) {
+            actualSum += arr[i];
+        }
+
+        int missingNumber = expectedSum - actualSum;
+
+        System.out.println("The missing number is : " + missingNumber);
+
+        sc.close();
+    }
+}
