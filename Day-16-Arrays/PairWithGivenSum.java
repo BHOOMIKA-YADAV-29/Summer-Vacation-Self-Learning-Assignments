68047d7edda38cd9d32440d3168446ea680fefa7
Bhoomika Yadav <25112cn089@glbitm.ac.in>
2026-06-29 00:43:24 +0530

    Added PairWithGivenSum program
    This program finds pairs of elements whose sum is equal to a given value.

--- Day-16-Arrays/PairWithGivenSum.java
+++ Day-16-Arrays/PairWithGivenSum.java
@@ -1,1 +1,42 @@
+import java.util.*;
 
+// Write a program to Find pair with given sum.
+
+public class PairWithGivenSum {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the size of the array :");
+        int size = sc.nextInt();
+
+        int[] arr = new int[size];
+
+        System.out.println("Enter the array elements :");
+
+        for ( int i = 0; i < size; i++ ) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Enter the target sum :");
+        int target = sc.nextInt();
+
+        boolean found = false;
+
+        for ( int i = 0; i < size; i++ ) {
+            for ( int j = i + 1; j < size; j++ ) {
+                if ( arr[i] + arr[j] == target ) {
+                    System.out.println("Pair found: " + arr[i] + " and " + arr[j]);
+                    found = true;
+                }
+            }
+        }
+
+        if (!found) {
+            System.out.println("No pair found");
+        }
+
+        sc.close();
+    }
+}
