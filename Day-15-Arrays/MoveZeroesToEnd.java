2d580902295f2062f236a1495b091488a6677649
Bhoomika Yadav <25112cn089@glbitm.ac.in>
2026-06-29 00:25:39 +0530

    Added MoveZeroesToEnd program
    This program moves all zeroes in an array to the end.

--- Day-15-Arrays/MoveZeroesToEnd.java
+++ Day-15-Arrays/MoveZeroesToEnd.java
@@ -1,1 +1,44 @@
+import java.util.*;
 
+// Write a program to Move zeroes to end.
+
+public class MoveZeroesToEnd {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the size of the array :");
+        int size = sc.nextInt();
+
+        int[] arr = new int[size];
+
+        System.out.println("Enter the array elements :");
+
+        for ( int i = 0; i < size; i++ ) {
+            arr[i] = sc.nextInt();
+        }
+
+        int index = 0;
+
+        for ( int i = 0; i < size; i++ ) {
+            if (arr[i] != 0) {
+                arr[index] = arr[i];
+                index++;
+            }
+        }
+
+        while ( index < size ) {
+            arr[index] = 0;
+            index++;
+        }
+
+        System.out.println("Array after moving zeroes to the end :");
+
+        for ( int i = 0; i < size; i++ ) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
+    }
+}
