6633346cab11cddc3a914ee05e4afa864ad0a2f6
Bhoomika Yadav <25112cn089@glbitm.ac.in>
2026-06-29 01:29:47 +0530

    Added DescendingSort program
    This program sorts the elements of an array in descending order.

---
+++ Day-18-Arrays/DescendingSort.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+// Write a program to Sort array in descending order.
+
+public class DescendingSort {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the size of the array :");
+        int size = sc.nextInt();
+
+        int[] arr = new int[size];
+
+        System.out.println("Enter the array elements :");
+
+        for ( int i = 0; i < size; i++ ) {
+            arr[i] = sc.nextInt();
+        }
+
+        // Bubble Sort in Descending Order
+        for ( int i = 0; i < size - 1; i++ ) {
+            for ( int j = 0; j < size - i - 1; j++ ) {
+
+                if ( arr[j] < arr[j + 1] ) {
+
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+
+                }
+            }
+        }
+
+        System.out.println("Array in descending order :");
+
+        for ( int i = 0; i < size; i++ ) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
+    }
+}
