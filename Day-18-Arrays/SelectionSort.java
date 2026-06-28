6fa1d54f09e864a9a49cca406f6fd63f8ab85e3f
Bhoomika Yadav <25112cn089@glbitm.ac.in>
2026-06-29 01:20:26 +0530

    Added SelectionSort program
    This program sorts the elements of an array using Selection Sort.

---
+++ Day-18-Arrays/SelectionSort.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+// Write a program to Selection sort.
+
+public class SelectionSort {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the size of the array :");
+        int size = sc.nextInt();
+
+        int[] arr = new int[size];
+
+        System.out.println("Enter the array elements :");
+
+        for ( int i = 0; i < size; i++ ) {
+            arr[i] = sc.nextInt();
+        }
+
+        // Selection Sort
+        for ( int i = 0; i < size - 1; i++ ) {
+
+            int minIndex = i;
+
+            for ( int j = i + 1; j < size; j++ ) {
+                if ( arr[j] < arr[minIndex] ) {
+                    minIndex = j;
+                }
+            }
+
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+
+        System.out.println("Sorted array :");
+
+        for ( int i = 0; i < size; i++ ) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
+    }
+}
