99e09be079d8cfc626ad1022898df49d0f1b600c
Bhoomika Yadav <25112cn089@glbitm.ac.in>
2026-06-29 00:12:47 +0530

    Added ReverseArray program
    This program displays the elements of an array in reverse order.

--- Day-15-Arrays/ReverseArray.java
+++ Day-15-Arrays/ReverseArray.java
@@ -1,1 +1,30 @@
+import java.util.*;
 
+// Write a program to Reverse array.
+
+public class ReverseArray {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the size of the array :");
+        int size = sc.nextInt();
+
+        int[] arr = new int[size];
+
+        System.out.println("Enter the array elements :");
+
+        for ( int i = 0; i < size; i++ ) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Reversed array :");
+
+        for ( int i = size - 1; i >= 0; i-- ) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
+    }
+}
