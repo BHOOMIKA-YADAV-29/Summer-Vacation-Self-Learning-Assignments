65dfeeb27d5f9a739fd1d531112c2391fc4423ff
Bhoomika Yadav <25112cn089@glbitm.ac.in>
2026-06-29 11:22:34 +0530

    Added PalindromeString program
    This program checks whether a string is a palindrome or not.

--- Day-22-String-Programs/PalindromeString.java
+++ Day-22-String-Programs/PalindromeString.java
@@ -1,1 +1,29 @@
+import java.util.*;
 
+// Write a program to Check palindrome string.
+
+public class PalindromeString {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a string :");
+        String str = sc.nextLine();
+
+        String reverse = "";
+
+        for ( int i = str.length() - 1; i >= 0; i-- ) {
+            reverse += str.charAt(i);
+        }
+
+        if ( str.equals(reverse) ) {
+            System.out.println("The string is a palindrome");
+        } 
+        else {
+            System.out.println("The string is not a palindrome");
+        }
+
+        sc.close();
+    }
+}
