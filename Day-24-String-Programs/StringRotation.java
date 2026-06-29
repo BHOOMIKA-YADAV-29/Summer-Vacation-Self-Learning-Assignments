abb89790987fcede604cb3174af8af24ef2a5e5d
Bhoomika Yadav <25112cn089@glbitm.ac.in>
2026-06-30 01:18:30 +0530

    Added StringRotation program
    This program checks whether one string is a rotation of another string or not.

--- Day-24-String-Programs/StringRotation.java
+++ Day-24-String-Programs/StringRotation.java
@@ -1,1 +1,34 @@
+import java.util.*;
 
+// Write a program to Check string rotation.
+
+public class StringRotation {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the first string :");
+        String str1 = sc.nextLine();
+
+        System.out.println("Enter the second string :");
+        String str2 = sc.nextLine();
+
+        if ( str1.length() != str2.length() ) {
+            System.out.println("The strings are not rotations");
+            sc.close();
+            return;
+        }
+
+        String temp = str1 + str1;
+
+        if ( temp.contains(str2) ) {
+            System.out.println("The strings are rotations of each other");
+        } 
+        else {
+            System.out.println("The strings are not rotations");
+        }
+
+        sc.close();
+    }
+}
