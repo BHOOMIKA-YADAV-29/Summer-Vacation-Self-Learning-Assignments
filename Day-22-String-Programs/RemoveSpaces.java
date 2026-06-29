5729a31366c7bc6d0a4f2aede8da0e13eb095cf9
Bhoomika Yadav <25112cn089@glbitm.ac.in>
2026-06-29 11:50:02 +0530

    Added RemoveSpaces program
    This program removes all spaces from a string.

---
+++ Day-22-String-Programs/RemoveSpaces.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+// Write a program to Remove spaces from string.
+
+public class RemoveSpaces {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a string :");
+        String str = sc.nextLine();
+
+        String result = "";
+
+        for ( int i = 0; i < str.length(); i++ ) {
+
+            char ch = str.charAt(i);
+
+            if ( ch != ' ' ) {
+                result += ch;
+            }
+        }
+
+        System.out.println("String after removing spaces : " + result);
+
+        sc.close();
+    }
+}
