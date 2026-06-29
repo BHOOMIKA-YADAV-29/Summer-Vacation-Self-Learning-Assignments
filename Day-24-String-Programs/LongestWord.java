7466814f3a2329cda0ece5b708cbd5c1ab17dbde
Bhoomika Yadav <25112cn089@glbitm.ac.in>
2026-06-30 01:55:19 +0530

    Added LongestWord program
    This program finds the longest word in a sentence.

---
+++ Day-24-String-Programs/LongestWord.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+// Write a program to Find longest word. 
+
+public class LongestWord {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a sentence :");
+        String str = sc.nextLine();
+
+        String[] words = str.split(" ");
+
+        String longest = words[0];
+
+        for ( int i = 1; i < words.length; i++ ) {
+
+            if ( words[i].length() > longest.length() ) {
+                longest = words[i];
+            }
+        }
+
+        System.out.println("Longest word : " + longest);
+        System.out.println("Length : " + longest.length());
+
+        sc.close();
+    }
+}
