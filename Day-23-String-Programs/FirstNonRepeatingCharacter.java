17a7be5410fa50043151646720ba2247fc8b3b54
Bhoomika Yadav <25112cn089@glbitm.ac.in>
2026-06-29 12:22:25 +0530

    Added FirstNonRepeatingCharacter program
    This program finds the first non-repeating character in a string.

--- Day-23-String-Programs/FirstNonRepeatingCharacter.java
+++ Day-23-String-Programs/FirstNonRepeatingCharacter.java
@@ -1,1 +1,40 @@
+import java.util.*;
 
+// Write a program to Find first non-repeating character.
+
+public class FirstNonRepeatingCharacter {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a string :");
+        String str = sc.nextLine();
+
+        boolean found = false;
+
+        for ( int i = 0; i < str.length(); i++ ) {
+
+            int count = 0;
+
+            for ( int j = 0; j < str.length(); j++ ) {
+
+                if ( str.charAt(i) == str.charAt(j) ) {
+                    count++;
+                }
+            }
+
+            if ( count == 1 ) {
+                System.out.println("First non-repeating character : " + str.charAt(i));
+                found = true;
+                break;
+            }
+        }
+
+        if ( !found ) {
+            System.out.println("No non-repeating character found");
+        }
+
+        sc.close();
+    }
+}
