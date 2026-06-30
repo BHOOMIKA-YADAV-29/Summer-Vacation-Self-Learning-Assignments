750bbc4b1168ab56c2a242443b9487d2807dc99f
Bhoomika Yadav <25112cn089@glbitm.ac.in>
2026-06-30 13:43:14 +0530

    Added ArmstrongNumbersInRange program
    This program prints all Armstrong numbers within a given range.

--- Day-04-Number-Programs/ArmstrongInRange.java
+++ Day-04-Number-Programs/ArmstrongInRange.java
@@ -1,1 +1,49 @@
+import java.util.*;
 
+// Write a program to print Armstrong numbers in a range.
+
+public class ArmstrongInRange {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the range of the Armstrong numbers: ");
+        int range = sc.nextInt();
+
+        System.out.println("Armstrong numbers are:");
+
+        for (int i = 0; i <= range; i++) {
+
+            if (i == 0) {
+                System.out.print("0 ");
+                continue;
+            }
+
+            int numberOfDigits = 0;
+            int sum = 0;
+            int originalNumber = i;
+
+            // Count the number of digits
+            while (originalNumber > 0) {
+                numberOfDigits++;
+                originalNumber /= 10;
+            }
+
+            originalNumber = i;
+
+            // Calculate the Armstrong sum
+            while (originalNumber > 0) {
+                int digit = originalNumber % 10;
+                sum += (int) Math.pow(digit, numberOfDigits);
+                originalNumber /= 10;
+            }
+
+            if (i == sum) {
+                System.out.print(i + " ");
+            }
+        }
+
+        sc.close();
+    }
+}
