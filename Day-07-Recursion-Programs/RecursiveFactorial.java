4203015a4db1acd127ef6e6b7f00e0f9c5d87436
Bhoomika Yadav <25112cn089@glbitm.ac.in>
2026-06-30 14:01:12 +0530

    Added RecursiveFactorial program
    This program calculates the factorial of a number using recursion.

--- Day-07-Recursion-Programs/RecursiveFactorial.java
+++ Day-07-Recursion-Programs/RecursiveFactorial.java
@@ -1,1 +1,27 @@
+import java.util.*;
 
+// Write a program to find factorial using recursion.
+
+public class RecursiveFactorial {
+
+    public static int factorial(int number) {
+
+        if (number == 0 || number == 1) {
+            return 1;
+        }
+
+        return number * factorial(number - 1);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        System.out.println("Factorial = " + factorial(number));
+
+        sc.close();
+    }
+}
