5abf00ffa580c8d327798e3c448f62047aaf5b25
Bhoomika Yadav <25112cn089@glbitm.ac.in>
2026-06-30 14:06:10 +0530

    Added RecursiveSumOfDigits program
    This program finds the sum of digits of a number using recursion.

--- Day-07-Recursion-Programs/RecursiveSumOfDigits.java
+++ Day-07-Recursion-Programs/RecursiveSumOfDigits.java
@@ -1,1 +1,27 @@
+import java.util.*;
 
+// Write a program to find the sum of digits using recursion.
+
+public class RecursiveSumOfDigits {
+
+    public static int sumOfDigits(int number) {
+
+        if (number == 0) {
+            return 0;
+        }
+
+        return (number % 10) + sumOfDigits(number / 10);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        System.out.println("Sum of digits = " + sumOfDigits(number));
+
+        sc.close();
+    }
+}
