4b4030556009c64c34b55ceeb884ed0b4687c9a7
Bhoomika Yadav <25112cn089@glbitm.ac.in>
2026-06-30 13:50:10 +0530

    Added BinaryToDecimal program
    This program converts a binary number into its decimal equivalent.

--- Day-06-Bit-Manipulation/BinaryToDecimal.java
+++ Day-06-Bit-Manipulation/BinaryToDecimal.java
@@ -1,1 +1,31 @@
+import java.util.*;
 
+// Write a program to convert binary to decimal.
+
+public class BinaryToDecimal {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a binary number: ");
+        int binary = sc.nextInt();
+
+        int decimal = 0;
+        int power = 0;
+
+        while (binary > 0) {
+
+            int digit = binary % 10;
+
+            decimal = decimal + (digit * (int) Math.pow(2, power));
+
+            power++;
+            binary = binary / 10;
+        }
+
+        System.out.println("Decimal number: " + decimal);
+
+        sc.close();
+    }
+}
