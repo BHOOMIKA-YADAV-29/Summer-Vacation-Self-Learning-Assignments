3e1b41f9a7d9db5f94bcb3ca1c69ee33b52b285b
Bhoomika Yadav <25112cn089@glbitm.ac.in>
2026-06-30 14:03:25 +0530

    Added RecursiveFibonacci program
    This program prints the Fibonacci series using recursion.

--- Day-07-Recursion-Programs/RecursiveFibonacci.java
+++ Day-07-Recursion-Programs/RecursiveFibonacci.java
@@ -1,1 +1,35 @@
+import java.util.*;
 
+// Write a program to print Fibonacci series using recursion.
+
+public class RecursiveFibonacci {
+
+    public static int fibonacci(int number) {
+
+        if (number == 0) {
+            return 0;
+        }
+
+        if (number == 1) {
+            return 1;
+        }
+
+        return fibonacci(number - 1) + fibonacci(number - 2);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of terms: ");
+        int terms = sc.nextInt();
+
+        System.out.println("Fibonacci Series:");
+
+        for (int i = 0; i < terms; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+
+        sc.close();
+    }
+}
