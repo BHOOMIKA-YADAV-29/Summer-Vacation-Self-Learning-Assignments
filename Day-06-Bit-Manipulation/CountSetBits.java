0ad13104f3ed5b7042239c2b35b7dc71a2c9e6de
Bhoomika Yadav <25112cn089@glbitm.ac.in>
2026-06-30 13:52:12 +0530

    Added CountSetBits program
    This program counts the number of set bits (1s) in the binary representation of a number.

--- Day-06-Bit-Manipulation/CountSetBits.java
+++ Day-06-Bit-Manipulation/CountSetBits.java
@@ -1,1 +1,29 @@
+import java.util.*;
 
+// Write a program to count set bits in a number.
+
+public class CountSetBits {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        int count = 0;
+
+        while (number > 0) {
+
+            if (number % 2 == 1) {
+                count++;
+            }
+
+            number = number / 2;
+        }
+
+        System.out.println("Number of set bits: " + count);
+
+        sc.close();
+    }
+}
